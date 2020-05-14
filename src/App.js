ede115ae4342289127dc1c3cfd5fe4462d0b8459
Łukasz Michalak <lukasz57570@gmail.com>
2020-05-14 19:21:13 +0200

    Configured eslint, prettier and husky pree-commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import styled from 'styled-components';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './theme/GlobalStyle';
 import theme from './theme/mainTheme';
-import styled from 'styled-components';
 
 const Button = styled.button`
   background-color: ${({ theme }) => theme.red || 'white'};
