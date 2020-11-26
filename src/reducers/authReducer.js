aee41919783c4ef8c57b79079fa7a354e9ffc6eb
Łukasz Michalak <lukasz.michalak96@outlook.com>
2020-11-26 15:41:14 +0100

    merged

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,24 @@
+import { SET_USER_IS_SIGNED_IN, SET_USER_IS_SIGNED_OUT } from '../actions/types';
+
+const initialState = {
+  synced: false,
+  user: null,
+};
+
+const authReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case SET_USER_IS_SIGNED_IN:
+      return {
+        synced: true,
+        user: payload,
+      };
+
+    case SET_USER_IS_SIGNED_OUT:
+      return { user: null };
+
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
